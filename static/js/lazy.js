e16893005195377b8a8dea803c6c6d54640ace40
grassblock <hi@imgb.space>
2024-06-18 16:48:35 +0800

    20240618 - add async load (thx to: https://albertarmea.com/post/async-load-hugo/)

---
+++ static/js/lazy.js
@@ -0,0 +1,70 @@
+// TODO: 'use strict'
+function loadPage(newUrl) {
+    const content = document.getElementById('main-content');
+    content.classList.add('fade-in');
+    content.classList.remove('fade-in');
+    setTimeout(()=> {content.classList.remove('fade-out')}, 200)
+    setTimeout(() => {sendRequest(newUrl)}, 100);
+    setTimeout(() => {content.classList.add('fade-in')}, 200)
+}
+
+function sendRequest(newUrl) {
+    const httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState !== XMLHttpRequest.DONE)
+            return;
+
+        // TODO: UI for this error
+        const newDocument = httpRequest.responseXML;
+        if (newDocument === null)
+            return;
+
+        // TODO: UI for this error
+        const newContent = httpRequest.responseXML.getElementById("main-content");
+        if (newContent === null)
+            return;
+
+        document.title = newDocument.title;
+
+        const contentElement = document.getElementById("main-content");
+        contentElement.replaceWith(newContent);
+    }
+
+    httpRequest.responseType = "document";
+    httpRequest.open("GET", newUrl);
+    httpRequest.send();
+
+
+}
+
+window.onload = function() {
+    // Make links load asynchronously
+    document.querySelector("body").addEventListener("click", function(event) {
+        // History API needed to make sure back and forward still work
+        if (history === null)
+            return;
+
+        // External links should instead open in a new tab
+        const newUrl = event.target.getAttribute("href") || (event.target.parentElement && event.target.parentElement.getAttribute("href"));
+        const domain = window.location.origin;
+        const isRelativeUrl = newUrl.startsWith('/') || newUrl.startsWith('.');
+        if (newUrl.startsWith('#'))
+            return;
+        if (!isRelativeUrl && (typeof domain !== "string" || newUrl.search(domain) !== 0)) {
+            event.preventDefault();
+            window.open(newUrl, "_blank");
+        } else {
+            event.preventDefault();
+            const content = document.getElementById('main-content');
+            content.classList.add('fade-out');
+            content.addEventListener('transitionend', function() {
+            loadPage(newUrl);
+            history.pushState(null /*stateObj*/, "" /*title*/, newUrl);
+            }, {once:true})
+        }
+    });
+}
+
+window.onpopstate = function(event) {
+    loadPage(window.location);
+}
