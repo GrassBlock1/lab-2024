667ac282ea36bdbfc37a22808a0ea257579e43d1
grassblock <hi@imgb.space>
2024-06-18 20:47:16 +0800

    20240618-2 - refactor: zero load async

--- static/js/lazy.js
+++ static/js/lazy.js
@@ -1,68 +1,81 @@
-// TODO: 'use strict'
-function loadPage(newUrl) {
-    sendRequest(newUrl)
+'use strict'
+// this loads page main element with a url and try to browse like a normal page
+async function loadPage(newUrl) {
+    const content = document.getElementById('main-content');
+    const response = await fetch(newUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'text/html'
+        }
+    });
+    const text = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(text, 'text/html');
+    content.innerHTML = doc.querySelector('#main-content').innerHTML;
+    document.title = doc.title;
+    history.pushState(null, doc.title, newUrl); // write browser history
 }
 
-function sendRequest(newUrl) {
-    const httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState !== XMLHttpRequest.DONE)
-            return;
-
-        // TODO: UI for this error
-        const newDocument = httpRequest.responseXML;
-        if (newDocument === null)
-            return;
-
-        // TODO: UI for this error
-        const newContent = httpRequest.responseXML.getElementById("main-content");
-        if (newContent === null)
-            return;
-
-        document.title = newDocument.title;
-
-        const contentElement = document.getElementById("main-content");
-        contentElement.replaceWith(newContent);
+// reinit scripts needed with specific element that is newly loaded,like aplayer.
+function reinitScript() {
+    const loadCommentButton = document.getElementById("load-comment");
+    if (loadCommentButton) {
+        const script = document.createElement("script");  // create a script DOM node
+        script.src = "/js/comments.js";  // set its src to the provided URL
+        const comment = document.getElementById("comments")
+        comment.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
     }
 
-    httpRequest.responseType = "document";
-    httpRequest.open("GET", newUrl);
-    httpRequest.send();
-
-
+    const guestbookElement = document.getElementById("Comments")
+    if (guestbookElement) {
+        const script = document.createElement("script");  // create a script DOM node
+        script.src = "https://gb-comments.zeabur.app/dist/Artalk.js";  // set its src to the provided URL
+        const comment = document.getElementsByTagName("section")[1]
+        comment.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+    }
 }
 
 window.onload = function() {
     // Make links load asynchronously
-    document.querySelector("body").addEventListener("click", function(event) {
+    document.querySelector("body").addEventListener("click", async function (event) {
         // History API needed to make sure back and forward still work
         if (history === null)
             return;
 
         // External links should instead open in a new tab
+        // for these elements don't like to use <a> standard
         const newUrl = event.target.getAttribute("href") || (event.target.parentElement && event.target.parentElement.getAttribute("href"));
+        if (!newUrl) return;
         const domain = window.location.origin;
+        // relative URLs seems to be more common for in-site link, so should be processed
         const isRelativeUrl = newUrl.startsWith('/') || newUrl.startsWith('.');
+        // skip with title anchor, they shouldn't use these
         if (newUrl.startsWith('#'))
             return;
         if (!isRelativeUrl && (typeof domain !== "string" || newUrl.search(domain) !== 0)) {
             event.preventDefault();
             window.open(newUrl, "_blank");
         } else {
-            event.preventDefault();
+            event.preventDefault()
             const content = document.getElementById('main-content');
+            // animations, load while getting content
             content.classList.add('fade-out');
+            await new Promise(resolve => {
+                content.addEventListener('transitionend', resolve, {once: true});
+            });
+            await loadPage(newUrl);
+            content.classList.remove('fade-out');
+            content.classList.add('fade-in');
+
             content.addEventListener('transitionend', function() {
-            loadPage(newUrl);
-            history.pushState(null /*stateObj*/, "" /*title*/, newUrl);
-            }, {once:true})
+                content.classList.remove('fade-in');
+            }, {once: true});
+
+            reinitScript();
         }
     });
 }
 
-window.onpopstate = function(event) {
-    loadPage(window.location);
-    const content = document.getElementById('main-content');
-    setTimeout(()=> {content.classList.remove('fade-out')}, 250)
-    setTimeout(() => {content.classList.add('fade-in')}, 250)
-}
+window.addEventListener('popstate', async function() {
+    await loadPage(location.pathname);
+});
