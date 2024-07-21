1ae3ce76044b68f4d208c6075adb98f1d46d86ab
grassblock <hi@imgb.space>
2024-07-21 11:05:38 +0800

    20240721 - changed the cleanup.sh for microblog support

--- cleanup.sh
+++ cleanup.sh
@@ -2,24 +2,28 @@
 
 # 指定需要检测的目录
 TARGET_DIR="./content/posts"
+DEST_DIR="./micro/content/posts"
 
-# 遍历目标目录的所有子目录
-for SUBDIR in "$TARGET_DIR"/*; do
-    if [ -d "$SUBDIR" ]; then
-        # 遍历子目录中的所有文件
-        for FILE in "$SUBDIR"/*; do
-            # 获取文件名
-            FILENAME=$(basename "$FILE")
-            EXTENSION="${FILENAME##*.}"
-
-            if [[ "$EXTENSION" == "md" && "$FILENAME" != "index.md" ]]; then
-                NEWFILE="$SUBDIR/index.md"
-                echo "Renaming $FILE to $NEWFILE"
-                mv "$FILE" "$NEWFILE"
-            else
-                echo "Nothing to do with $SUBDIR/$FILENAME ...Moving on to next."
-            fi
-        done
+# Iterate over all files in subdirectories of TARGET_DIR
+find "$TARGET_DIR" -type f -name "*.md" | while read -r FILE; do
+    # Check if the 3rd line contains 'is_microblog: true'
+    if sed -n '3p' "$FILE" | grep -q 'is_microblog: true'; then
+        # Construct the destination path, preserving the subdirectory structure
+        DEST_PATH="$DEST_DIR${FILE#"$TARGET_DIR"}"
+        # Move the file
+        mv "$FILE" "$DEST_PATH"
+        echo "Moved $FILE to $DEST_PATH"
+    fi
+    TARGET_SUBDIR=$(dirname "$FILE")
+    NEW_FILE="$TARGET_SUBDIR/index.md"
+    INDEX_FILE="$TARGET_SUBDIR/_index.md"
+    INDEX_EN_FILE="$TARGET_SUBDIR/_index.md"
+    if [[ -f "$NEW_FILE" || -f "$INDEX_FILE" || -f "$INDEX_EN_FILE" ]]; then
+        echo "File $NEW_FILE already moved correctly. Skipping."
+        continue
+    else
+        echo "Renaming $FILE to $NEW_FILE"
+        mv "$FILE" "$NEW_FILE"
     fi
 done
 
