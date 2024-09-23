aeef3eb3128642f6d31596e8b78def8ab31d8f00
grassblock <hi@imgb.space>
2024-09-23 18:05:44 +0800

    fix: build with tinacms

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-tinacms build && hugo --minify --gc && "$SCRIPT_DIR"/encrypt
+pnpx @tinacms/ build && hugo --minify --gc && "$SCRIPT_DIR"/encrypt
