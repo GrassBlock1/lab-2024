b253f2991f5833b24779a86f5d7dff604ed12559
grassblock <hi@imgb.space>
2024-09-23 17:53:21 +0800

    feat: add tina.io cms

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-hugo --minify --gc && "$SCRIPT_DIR"/encrypt
+tinacms build && hugo --minify --gc && "$SCRIPT_DIR"/encrypt
---
+++ tina/config.ts
@@ -0,0 +1,129 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "master";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "img/uploads",
+      publicFolder: "assets",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "文章",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+                type: "string",
+                name: "description",
+                label: "Description",
+                required: true,
+          },
+          {
+                type: "datetime",
+                name: "date",
+                label: "Date",
+                required: true,
+          },
+          {
+                type: "image",
+                name: "coverImage",
+                label: "Cover Image",
+                required: true,
+          },
+          {
+            type: "string",
+            label: "Tags",
+            name: "tags",
+            list: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "page",
+        label: "页面",
+        path: "content/pages",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            required: true,
+          },
+          {
+            type: "image",
+            name: "coverImage",
+            label: "Cover Image",
+            required: true,
+          },
+          {
+            type: "string",
+            label: "Tags",
+            name: "tags",
+            list: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+  search: {
+    tina: {
+      indexerToken: process.env.TINA_SEARCH_TOKEN,
+      stopwordLanguages: ['zho','eng'],
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100,
+  },
+});
