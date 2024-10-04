11a5158acfcd1db0a3431916aee397e424ba0911
grassblock <hi@imgb.space>
2024-10-04 11:00:26 +0800

    feat: try to use worker wrangler cli to build site.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 #pnpx @tinacms/cli build && hugo --minify --gc && "$SCRIPT_DIR"/encrypt
-hugo --minify --gc && "$SCRIPT_DIR"/encrypt
+hugo --minify --gc "$@" && "$SCRIPT_DIR"/encrypt && echo "build complete"
